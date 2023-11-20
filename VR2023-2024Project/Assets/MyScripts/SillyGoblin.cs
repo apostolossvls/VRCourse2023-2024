a5167066d87e68be7fac18032e40162e47b331b5
themisp3 <themis.panayiotopoulos3@gmail.com>
2023-11-20 17:58:59 +0200

    Lesson 8

--- VR2023-2024Project/Assets/MyScripts/SillyGoblin.cs
+++ VR2023-2024Project/Assets/MyScripts/SillyGoblin.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class SillyGoblin : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Vector3 eyes1;
+    [SerializeField] private Transform eyes2;
     [SerializeField] private Transform target;
 
     // Start is called before the first frame update
@@ -14,13 +17,21 @@ public class SillyGoblin : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         bool eyeContact = false;
+
         RaycastHit hit;
-        Vector3 direction = target.position - transform.position;
+        Vector3 difference = (target.position - eyes2.position);
+        Vector3 direction = difference.normalized;
         direction.Normalize();
-        if (Physics.Raycast(transform.position + Vector3.up, direction, out hit, Mathf.Infinity))
+        Debug.DrawLine(transform.position, transform.position + direction);
+        if (Physics.Raycast(
+            eyes2.position,
+            direction,
+            out hit,
+            Mathf.Infinity)
+            )
         {
             if (hit.collider.transform == target)
             {
