740eb1ef64cd2442dc96ce18bb591320617044ba
themisp3 <themis.panayiotopoulos3@gmail.com>
2023-12-08 16:08:04 +0200

    Lesson 10

---
+++ VR2023-2024Project/Assets/MyScripts/NPCPatrol.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCPatrol : MonoBehaviour
+{
+    public Transform[] targets;
+    int targetIndex;
+    NavMeshAgent agent;
+    public Animator animator;
+    float timer;
+    float waitTime;
+    float waitTimeMin = 6f;
+    float waitTimeMax = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        timer = 0;
+
+        targetIndex = -1;
+        GoNext();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < agent.stoppingDistance && !agent.pathPending)
+        {
+            animator.SetBool("Walking", false);
+
+            timer = timer + Time.deltaTime;
+
+            if (timer >= waitTime)
+            {
+                GoNext();
+            }
+        }
+
+    }
+
+    void GoNext()
+    {
+        timer = 0;
+
+        waitTime = Random.Range(waitTimeMin, waitTimeMax);
+
+        //i
+        targetIndex = (targetIndex + 1) % targets.Length;
+
+        //animation
+        animator.SetBool("Walking", true);
+
+        agent.SetDestination(targets[targetIndex].position);
+    }
+}
